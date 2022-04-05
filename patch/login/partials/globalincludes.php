3dcc504ce47e4c2bd077b1a9d2a190ff49fd4fb2
c00cjz00 <summerhill001@gmail.com>
2022-04-05 11:10:37 +0800

    Update globalincludes.php

--- patch/login/partials/globalincludes.php
+++ patch/login/partials/globalincludes.php
@@ -1,24 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="<?=$this->base_url;?>/login/css/favicon.ico">
-    
-	<title><?=$title;?></title>
-        
-	<!-- Bootstrap core CSS -->
-    <link href="<?=$this->base_url;?>/vendor/components/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-	
-	<!-- Custom styles for this template -->
-    <link href="<?=$this->base_url;?>/login/css/main.css" rel="stylesheet" media="screen">
-    <link href="<?=$this->base_url;?>/login/css/footer.css" rel="stylesheet">
+ <head>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <meta name="description" content="">
+ <meta name="author" content="">
+ <link rel="icon" href="<?=$this->base_url;?>/login/css/favicon.ico">
+ <title><?=$title;?></title>
+ 
+ <!-- Bootstrap core CSS -->
+ <link href="<?=$this->base_url;?>/vendor/components/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+ 
+ <!-- bootstrap & fontawesome -->
+ <link rel="stylesheet" href="<?=$this->base_url;?>/vendor/components/font-awesome/css/font-awesome.min.css">
+ 
+ <!-- text fonts -->
+ <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">	
+	 
+ <!-- Custom styles for this template -->
+ <link href="<?=$this->base_url;?>/login/css/main.css" rel="stylesheet" media="screen">
+ <link href="<?=$this->base_url;?>/login/css/footer.css" rel="stylesheet">
+ <style type="text/css">
+  .btn-group-xs > .btn, .btn-xs {
+   padding: 1px 5px;
+   font-size: 12px;
+   line-height: 1.5;
+   border-radius: 3px;
+  }
+ </style>
+ <!-- Bootstrap core JavaScript -->
+ <!-- Placed at the end of the document so the pages load faster -->
+ <script src="<?=$this->base_url;?>/vendor/components/jquery/jquery.min.js" type="application/javascript"></script>
+ <script>window.jQuery || document.write('<script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+ <script src="<?=$this->base_url;?>/vendor/components/bootstrap/js/bootstrap.bundle.min.js" type="application/javascript"></script>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-	<script src="<?=$this->base_url;?>/vendor/components/jquery/jquery.min.js" type="application/javascript"></script>
-    <script src="<?=$this->base_url;?>/vendor/components/bootstrap/js/bootstrap.min.js" type="application/javascript"></script>
 
